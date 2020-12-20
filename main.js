1874fce528e7f8ac23492320be7a3422ddee545f
Boris Krasko <boris.krasko@live.com>
2020-12-20 22:12:09 +0100

    refactor: change file structure

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ let isModalActive = false;
 for (let i = 0; i < fullScreenBtn.length; i++) {
 	fullScreenBtn[i].addEventListener('click', function () {
 	 	modalWindow.classList.toggle('active');
-	 	fullScreenBtn[i].innerHTML = '<img src="screen.svg">';
+	 	fullScreenBtn[i].innerHTML = '<img src="assets/icons/screen.svg">';
 	 	isModalActive = !isModalActive;
 	 	if (isModalActive === true) {
 	 		container[i].classList.add('full');
 	 		modalWindow.appendChild(container[i]);
 	 	} else {
-	 		fullScreenBtn[i].innerHTML = '<img src="fullscreen.svg">';
+	 		fullScreenBtn[i].innerHTML = '<img src="assets/icons/fullscreen.svg">';
 	 		container[i].classList.remove('full');
 	 		modalWindow.removeChild(container[i]);
 	 		document.body.appendChild(container[i]);
